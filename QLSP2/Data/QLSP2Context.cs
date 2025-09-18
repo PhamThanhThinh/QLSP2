d65ddb8c9cd0c5ea7bc6a5d479a3cae68afa202c
thinh <thinhqn999@gmail.com>
2025-09-18 21:57:34 +0700

    Add project files.

---
+++ QLSP2/Data/QLSP2Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using QLSP2.Models;
+
+namespace QLSP2.Data
+{
+  public class QLSP2Context : DbContext
+  {
+    public QLSP2Context(DbContextOptions<QLSP2Context> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<QLSP2.Models.Product> Product { get; set; } = default!;
+  }
+}
---
+++ QLSP2/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿namespace QLSP2.Models
+{
+  public class Product
+  {
+    // thường thường dùng int
+    //public int Id { get; set; }
+    // dùng Guid
+    // ví dụ: 123xxx-456abc...
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+
+    // giá - đô la - usd
+    // ví dụ: 1usd, 1.5usd
+    // c# double -> sql float
+    public double Price { get; set; }
+
+    // số lượng
+    public int Quantity { get; set; }
+  }
+}
